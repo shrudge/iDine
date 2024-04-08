99c29811631f356e6f27bb20526e32c88577cb19
Meet Balani <74920401+shrudge@users.noreply.github.com>
2024-04-08 23:58:34 +0530

    Added Cart page , made a Tab bar view

---
+++ iDine/OrderView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Meet Balani on 08/04/24.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderView()
+}
