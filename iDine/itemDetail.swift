d0a48d44661be61edde65e7996d9f2d12668a7cc
Meet Balani <74920401+shrudge@users.noreply.github.com>
2024-04-08 21:22:42 +0530

    Added tem detail view and created order taking mechanism

---
+++ iDine/itemDetail.swift
@@ -0,0 +1,18 @@
+//
+//  itemDetail.swift
+//  iDine
+//
+//  Created by Meet Balani on 08/04/24.
+//
+
+import SwiftUI
+
+struct itemDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    itemDetail()
+}
