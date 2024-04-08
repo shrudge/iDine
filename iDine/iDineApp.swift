a71b91bc0d5c98389d701563da14e9e834204c7e
Meet Balani <74920401+shrudge@users.noreply.github.com>
2024-04-08 10:01:04 +0530

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Meet Balani on 08/04/24.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
